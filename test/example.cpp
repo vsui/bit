fee8e40ed7b832a2a667a866183309bb7e6ff397
Victor Sui <victorsui11@gmail.com>
2019-09-05 22:12:30 -0500

    Fix example tests

--- test/example.cpp
+++ test/example.cpp
@@ -1,20 +1,27 @@
-#include "bit_iterator.hpp"
+#include "bit.hpp"
 #include "gtest/gtest.h"
 #include <algorithm>
 #include <cstddef>
 #include <vector>
 
-template <typename... Ts>
-std::vector<std::byte> make_bytes(Ts &&... args) noexcept {
-  return {std::byte(std::forward<Ts>(args))...};
+TEST(IsPermutationTest, Simple) {
+  auto seq1 = std::vector<unsigned char>{0b0000'0000, 0b1111'1111};
+  auto seq2 = std::vector<unsigned char>{0b0101'0101, 0b0101'0101};
+
+  bit::bit_iterator<decltype(seq1.begin())> begin1(seq1.begin());
+  bit::bit_iterator<decltype(seq1.end())> end1(seq1.end());
+  bit::bit_iterator<decltype(seq2.begin())> begin2(seq2.begin());
+
+  EXPECT_TRUE(std::is_permutation(begin1, end1, begin2));
 }
 
-TEST(IsPermutationTest, Simple) {
-  auto seq1 = make_bytes(0b0000'0000, 0b1111'1111);
-  auto seq2 = make_bytes(0b0101'0101, 0b0101'0101);
+TEST(IsPermutationTest, UnalignedWords) {
+  auto seq1 = std::vector<unsigned char>{0b0000'0000, 0b1111'1111};
+  auto seq2 = std::vector<unsigned char>{0b0101'0101, 0b0101'0101};
 
-  bit::bit_iterator it1(seq1);
-  bit::bit_iterator it2(seq2);
+  bit::bit_iterator<decltype(seq1.begin())> begin1(seq1.begin());
+  bit::bit_iterator<decltype(seq1.end())> end1(seq1.end());
+  bit::bit_iterator<decltype(seq2.begin())> begin2(seq2.begin());
 
-  EXPECT_TRUE(std::is_permutation(seq1.begin(), seq1.end(), seq2.begin()));
+  EXPECT_TRUE(std::is_permutation(begin1 + 1, end1 - 1, begin2 + 2));
 }
